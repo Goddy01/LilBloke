11f52ac47656bd9fe6d8fbfbdff2dad63719e471
Toshihiro <adigungodwin2@gmail.com>
2022-11-26 19:15:34 +0100

    added accounts.models.AccountManager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User, BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+class AccountManager(BaseUserManager):
+    """Creates and saves a user with the given details"""
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("The user must provide an email")
+        if not username:
+            raise ValueError("The user must provide a username")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        """Creates and saves a superuser with the given details"""
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
