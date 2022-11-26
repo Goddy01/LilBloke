34d71e11caf27ac92aa5bdb62d51d1f9ef9aebf5
Toshihiro <adigungodwin2@gmail.com>
2022-11-26 22:32:19 +0100

    added SignUpForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 from .models import UserAccount
+from django.contrib.auth import authenticate
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = UserAccount
+        fields = ['username', 'email', 'password1', 'password2']
 
 class SignInForm(forms.ModelForm):
     class Meta:
         model = UserAccount
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['email', ]
+
+    def clean(self):
+        email = self.cleaned_data.get('email').lower()
+        password = self.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise forms.ValidationError('User does not exist.')
