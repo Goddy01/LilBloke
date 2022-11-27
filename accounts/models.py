a4e963d4083c7dee64857c8e3f65f48a5f4e07f0
Toshihiro <adigungodwin2@gmail.com>
2022-11-27 14:55:17 +0100

    added func to send account_signup confirmation mail

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+class AccountManager(BaseUserManager):
+    """Creates and saves a user with the given details"""
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("The user must provide an email")
+        if not username:
+            raise ValueError("The user must provide a username")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        """Creates and saves a superuser with the given details"""
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+class UserAccount(AbstractBaseUser):
+    username =              models.CharField(unique=True, max_length=128, null=False)
+    email =                 models.EmailField(unique=True, null=False)
+    date_joined =           models.DateTimeField(auto_now_add=True)
+    is_active =             models.BooleanField(default=False)
+    signup_confirmation =   models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """Checks if the user has any permissions"""
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        """Checks if the user has permission to view the app 'app_label'"""
+        return True
+    def __str__(self):
+        return self.username
