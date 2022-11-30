9dcf44f5b54a35a884189cf3b205fb060b95de6b
Toshihiro <adigungodwin2@gmail.com>
2022-11-30 15:44:47 +0100

    added password_change temp

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,21 +11,21 @@ urlpatterns = [
     path('activate-account/<slug:uidb64>/<slug:token>/', views.activate_account, name='activate'),
     path('sign-out/', views.sign_out, name='sign_out'),
 
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="password/password_change_done.html"), name="password_change_done"),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name="accounts/password/password_change_done.html"), name="password_change_done"),
 
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="password_reset_done"),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password/password_reset_done.html"), name="password_reset_done"),
 
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name="password_reset_complete"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password/password_reset_complete.html'), name="password_reset_complete"),
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='password/password_reset_form.html', 
+        template_name='accounts/password/password_reset_form.html', 
         success_url = reverse_lazy('password_reset_complete'),
     ), name="password_reset_confirm"),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='password/password_reset.html',
-        email_template_name='password/password_reset_email.html',
-        subject_template_name='password/password_reset_subject.txt',
+        template_name='accounts/password/password_reset.html',
+        email_template_name='accounts/password/password_reset_email.html',
+        subject_template_name='accounts/password/password_reset_subject.txt',
         success_url = reverse_lazy('password_reset_done'),
     ), name='password_reset'),
 
