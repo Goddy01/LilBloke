083605d56b34b736f627c7402eaf87c0e1d6954d
Toshihiro <adigungodwin2@gmail.com>
2022-12-11 18:34:36 +0100

    displaying movies on tv series detail page

--- movie_app/settings.py
+++ movie_app/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'core',
+    'embed_video',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
