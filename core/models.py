8f5335c6fc840f17de9b4c3b62bdfcdc342c3794
Toshihiro <adigungodwin2@gmail.com>
2022-12-15 12:12:14 +0100

    created Comment Table

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from accounts.models import UserAccount
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    movie_id = models.IntegerField(blank=False, null=False)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
