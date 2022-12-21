21a3f03d969ac76a2778ffccc81b79fb3c5a7053
Toshihiro <adigungodwin2@gmail.com>
2022-12-21 11:20:31 +0100

    added LOGGING to prevent 400 error

--- movie_app/settings.py
+++ movie_app/settings.py
@@ -164,3 +164,37 @@ DEFAULT_FROM_EMAIL = 'Flixland Team <noreply@flixland.com>'
 # MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 AUTH_USER_MODEL = 'accounts.UserAccount'
+
+# TO PREVENT 400 ERROR
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'formatters': {
+    'verbose': {
+        'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+        'datefmt' : "%d/%b/%Y %H:%M:%S"
+    },
+    'simple': {
+        'format': '%(levelname)s %(message)s'
+    },
+},
+'handlers': {
+    'file': {
+        'level': 'DEBUG',
+        'class': 'logging.FileHandler',
+        'filename': 'mysite.log',
+        'formatter': 'verbose'
+    },
+},
+'loggers': {
+    'django': {
+        'handlers':['file'],
+        'propagate': True,
+        'level':'DEBUG',
+    },
+    'MYAPP': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
+}
